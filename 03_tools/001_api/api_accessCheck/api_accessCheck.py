301d0b47fbff8333cb2e85cd1685a9af685fb031
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2023-02-03 16:32:57 -0500

    add_auth_mode

--- 03_tools/001_api/api_accessCheck/api_accessCheck.py
+++ 03_tools/001_api/api_accessCheck/api_accessCheck.py
@@ -3,13 +3,14 @@ Desc    : This tool is to automatically check for API permissions
 Author  : bigb0ss
 Dev     :
     - (1) GET Request [Complete]
+        - You can include an access token into 'token.cfg' file; otherwise, it will be an unauthorized mode
     - (2) POST Request [Not Started]
-
 '''
 
 import requests
 import argparse
 import sys
+import configparser
 
 class Color:
     yellow = '\033[95m'
@@ -22,27 +23,40 @@ class Color:
 parser = argparse.ArgumentParser(description='[INFO] API Access Check')
 parser.add_argument('-u', '--url', help="Target URL (e.g., https://google.com/)")
 parser.add_argument('-f', '--file', help="API Endpoints")
+parser.add_argument('-t', '--token', help="API Authentication Token")
 args = parser.parse_args()
+
+""" Login Credentials """
+config = configparser.RawConfigParser()
+config.read("token.cfg")
+api_token = config.get('API_KEYS', 'token')
 
 def error():
     parser.print_help()
     sys.exit(1)
 
-def api_get_request(url, file):
+def api_get_request(url, file, api_token):
     # Reading API file + making requests
-    headers = {"user-agent":"hacker0ne: spicyb0ss"}
+    headers = {
+        "User-agent":"hacker0ne: spicyb0ss",
+        "Authorization": api_token 
+        }
+
+    api = []
 
     for i in open(file, "r"):
+        api .append(i.replace("\n", ""))
+
+    for i in api:
         r = requests.get(url+i, headers=headers)
-        print(f"[INFO] Testing: {i} Status Code:", r.status_code)
+        print(f"[INFO] Testing: {i} - Status Code:", r.status_code)
         if r.status_code == 200: 
             print(Color.green + "ACCESSABLE! --> " + url+i + Color.end)
     return
 
-
 if __name__ == '__main__':
     url = args.url if args.url != None else error()
     file = args.file if args.url != None else error()
-    api_get_request(url, file)
+    api_get_request(url, file, api_token)
 
 
